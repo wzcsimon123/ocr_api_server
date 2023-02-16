d7b0606ca61f4c4be88434e93df2407176e9c9a4
shanmite <shanmite@hotmail.com>
2023-02-16 12:36:00 +0800

    /ocr/url/text url=...

--- ocr_server.py
+++ ocr_server.py
@@ -5,6 +5,7 @@ import json
 
 import ddddocr
 from flask import Flask, request
+import requests
 
 parser = argparse.ArgumentParser(description="使用ddddocr搭建的最简api服务")
 parser.add_argument("-p", "--port", type=int, default=9898)
@@ -54,31 +55,35 @@ class Server(object):
 
     def slide(self, target_img: bytes, bg_img: bytes, algo_type: str):
         dddd = self.ocr or self.det or ddddocr.DdddOcr(ocr=False)
-        if algo_type == 'match':
+        if algo_type == "match":
             return dddd.slide_match(target_img, bg_img)
-        elif algo_type == 'compare':
+        elif algo_type == "compare":
             return dddd.slide_comparison(target_img, bg_img)
         else:
             raise Exception(f"不支持的滑块算法类型: {algo_type}")
 
+
 server = Server(ocr=args.ocr, det=args.det, old=args.old)
 
 
-def get_img(request, img_type='file', img_name='image'):
-    if img_type == 'b64':
-        img = base64.b64decode(request.get_data()) # 
-        try: # json str of multiple images
+def get_img(request, img_type="file", img_name="image"):
+    if img_type == "b64":
+        img = base64.b64decode(request.get_data())  #
+        try:  # json str of multiple images
             dic = json.loads(img)
             img = base64.b64decode(dic.get(img_name).encode())
-        except Exception as e: # just base64 of single image
+        except Exception as e:  # just base64 of single image
             pass
+    elif img_type == "url":
+        print(request.form["url"])
+        img = requests.get(request.form["url"]).content
     else:
         img = request.files.get(img_name).read()
     return img
 
 
-def set_ret(result, ret_type='text'):
-    if ret_type == 'json':
+def set_ret(result, ret_type="text"):
+    if ret_type == "json":
         if isinstance(result, Exception):
             return json.dumps({"status": 200, "result": "", "msg": str(result)})
         else:
@@ -86,19 +91,19 @@ def set_ret(result, ret_type='text'):
         # return json.dumps({"succ": isinstance(result, str), "result": str(result)})
     else:
         if isinstance(result, Exception):
-            return ''
+            return ""
         else:
             return str(result).strip()
 
 
-@app.route('/<opt>/<img_type>', methods=['POST'])
-@app.route('/<opt>/<img_type>/<ret_type>', methods=['POST'])
-def ocr(opt, img_type='file', ret_type='text'):
+@app.route("/<opt>/<img_type>", methods=["POST"])
+@app.route("/<opt>/<img_type>/<ret_type>", methods=["POST"])
+def ocr(opt, img_type="file", ret_type="text"):
     try:
         img = get_img(request, img_type)
-        if opt == 'ocr':
+        if opt == "ocr":
             result = server.classification(img)
-        elif opt == 'det':
+        elif opt == "det":
             result = server.detection(img)
         else:
             raise f"<opt={opt}> is invalid"
@@ -106,21 +111,23 @@ def ocr(opt, img_type='file', ret_type='text'):
     except Exception as e:
         return set_ret(e, ret_type)
 
-@app.route('/slide/<algo_type>/<img_type>', methods=['POST'])
-@app.route('/slide/<algo_type>/<img_type>/<ret_type>', methods=['POST'])
-def slide(algo_type='compare', img_type='file', ret_type='text'):
+
+@app.route("/slide/<algo_type>/<img_type>", methods=["POST"])
+@app.route("/slide/<algo_type>/<img_type>/<ret_type>", methods=["POST"])
+def slide(algo_type="compare", img_type="file", ret_type="text"):
     try:
-        target_img = get_img(request, img_type, 'target_img')
-        bg_img = get_img(request, img_type, 'bg_img')
+        target_img = get_img(request, img_type, "target_img")
+        bg_img = get_img(request, img_type, "bg_img")
         result = server.slide(target_img, bg_img, algo_type)
         return set_ret(result, ret_type)
     except Exception as e:
         return set_ret(e, ret_type)
 
-@app.route('/ping', methods=['GET'])
+
+@app.route("/ping", methods=["GET"])
 def ping():
     return "pong"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=args.port)
